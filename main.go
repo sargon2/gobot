da0e3a1734619124e76f6fcb1bf17507b26e58ef
David Besen <dbesen@gmail.com>
2021-01-31 08:07:35 -0700

    Get receiving messages working

--- main.go
+++ main.go
@@ -2,23 +2,86 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
+
+	"github.com/slack-go/slack/socketmode"
 
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 )
 
 func main() {
-	token := os.Getenv("SLACK_BOT_TOKEN")
-	// teamID := os.Getenv("SLACK_BOT_TEAM_ID")
-	channelID := os.Getenv("SLACK_BOT_CHANNEL_ID")
-	api := slack.New(token)
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	if appToken == "" {
 
-	_, _, err := api.PostMessage(
-		channelID,
-		slack.MsgOptionText("Hello", false),
-		slack.MsgOptionAsUser(true),
-	)
-	if err != nil {
-		fmt.Println(err)
 	}
+
+	if !strings.HasPrefix(appToken, "xapp-") {
+		fmt.Fprintf(os.Stderr, "SLACK_APP_TOKEN must have the prefix \"xapp-\".")
+	}
+
+	botToken := os.Getenv("SLACK_BOT_TOKEN")
+	if botToken == "" {
+		fmt.Fprintf(os.Stderr, "SLACK_BOT_TOKEN must be set.\n")
+		os.Exit(1)
+	}
+
+	if !strings.HasPrefix(botToken, "xoxb-") {
+		fmt.Fprintf(os.Stderr, "SLACK_BOT_TOKEN must have the prefix \"xoxb-\".")
+	}
+
+	api := slack.New(
+		botToken,
+		slack.OptionDebug(true),
+		slack.OptionLog(log.New(os.Stdout, "api: ", log.Lshortfile|log.LstdFlags)),
+		slack.OptionAppLevelToken(appToken),
+	)
+
+	client := socketmode.New(
+		api,
+		socketmode.OptionDebug(true),
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	go func() {
+		for evt := range client.Events {
+			switch evt.Type {
+			case socketmode.EventTypeConnecting:
+				fmt.Println("Connecting to Slack with Socket Mode...")
+			case socketmode.EventTypeConnectionError:
+				fmt.Println("Connection failed. Retrying later...")
+			case socketmode.EventTypeConnected:
+				fmt.Println("Connected to Slack with Socket Mode.")
+			case socketmode.EventTypeEventsAPI:
+				eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					fmt.Printf("Event not ok %+v\n", evt)
+					continue
+				}
+
+				fmt.Printf("Event received: %+v\n", eventsAPIEvent)
+
+				client.Ack(*evt.Request)
+
+				switch eventsAPIEvent.Type {
+				case slackevents.CallbackEvent:
+					innerEvent := eventsAPIEvent.InnerEvent
+					switch ev := innerEvent.Data.(type) {
+					case *slackevents.MessageEvent:
+						fmt.Printf("Message received: %+v", ev)
+					default:
+						fmt.Printf("Unsupported inner event type %T\n", innerEvent.Data)
+					}
+				default:
+					fmt.Printf("Unsupported Events API event type %s\n", eventsAPIEvent.Type)
+				}
+			default:
+				fmt.Fprintf(os.Stderr, "Unsupported event type received: %s\n", evt.Type)
+			}
+		}
+	}()
+
+	client.Run()
 }
