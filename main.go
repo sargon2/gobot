32b1081587b91303aafc5e20b80fbfe1ced66769
David Besen <dbesen@gmail.com>
2021-01-30 17:52:49 -0700

    Use environment for config

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/slack-go/slack"
 )
 
 func main() {
-	api := slack.New("YOUR_TOKEN_HERE")
-	// teamID := "T0EUDFVLM"    // solsar
-	channelID := "G6Z2PFJM8" // #vatcave
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	// teamID := os.Getenv("SLACK_BOT_TEAM_ID")
+	channelID := os.Getenv("SLACK_BOT_CHANNEL_ID")
+	api := slack.New(token)
+
 	_, _, err := api.PostMessage(
 		channelID,
 		slack.MsgOptionText("Hello", false),
