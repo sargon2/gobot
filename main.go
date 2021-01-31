b07bcd1348948d6811a8311d93c6aab80fc8c4fc
David Besen <dbesen@gmail.com>
2021-01-30 17:34:54 -0700

    Say hello

--- main.go
+++ main.go
@@ -8,5 +8,14 @@ import (
 
 func main() {
 	api := slack.New("YOUR_TOKEN_HERE")
-	fmt.Println("whatever", api)
+	// teamID := "T0EUDFVLM"    // solsar
+	channelID := "G6Z2PFJM8" // #vatcave
+	_, _, err := api.PostMessage(
+		channelID,
+		slack.MsgOptionText("Hello", false),
+		slack.MsgOptionAsUser(true),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
