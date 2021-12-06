a1fe3eb24e38eeff2ad9401dfdf2030be9e99c04
David Besen <dbesen@gmail.com>
2021-12-06 12:40:54 -0700

    Only return error in case of error in calc

--- internal/app/gobot/plugins/calc.go
+++ internal/app/gobot/plugins/calc.go
@@ -35,6 +35,7 @@ func (c *Calc) handleMessage(source *gobot.MessageSource, message string) {
 	result, err := c.client.GetShortAnswerQuery(message, wolfram.Imperial, 30)
 	if err != nil {
 		c.hub.Message(source, "Got an error: "+err.Error())
+		return
 	}
 	c.hub.Message(source, result)
 }
