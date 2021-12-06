f8b834eb4bd48b293e4820a39883ebfbcfa2a934
David Besen <dbesen@gmail.com>
2021-12-06 13:28:04 -0700

    Attempt to ignore retries

--- internal/app/gobot/lambda_event_processor.go
+++ internal/app/gobot/lambda_event_processor.go
@@ -48,6 +48,10 @@ func (s *LambdaEventProcessor) Message(source *MessageSource, m string) {
 
 func (s *LambdaEventProcessor) HandleEvent(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// fmt.Printf("Got request: %v\n", request)
+	if _, ok := request.Headers["X-Slack-Retry-Num"]; ok {
+		fmt.Println("X-Slack-Retry-Num set, aborting")
+		return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+	}
 
 	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(request.Body), slackevents.OptionNoVerifyToken())
 	if err != nil {
