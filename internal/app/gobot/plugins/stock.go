a3650dffbfe5eed266a3b9044d6e069e69981471
David Besen <dbesen@gmail.com>
2023-04-20 09:15:51 -0600

    Re-enable stocks

--- internal/app/gobot/plugins/stock.go
+++ internal/app/gobot/plugins/stock.go
@@ -27,13 +27,12 @@ func NewStock(hub *gobot.Hub, remember *Remember) *Stock {
 }
 
 func (p *Stock) handleStocks(source *gobot.MessageSource, message string) {
-	p.hub.Message(source, "bork bork bork")
-	// whatisResult, err := p.remember.Whatis("stocks")
-	// if err != nil {
-	// 	p.hub.Message(source, "Error: "+err.Error())
-	// 	return
-	// }
-	// p.handleStock(source, whatisResult.Value+" "+message)
+	whatisResult, err := p.remember.Whatis("stocks")
+	if err != nil {
+		p.hub.Message(source, "Error: "+err.Error())
+		return
+	}
+	p.handleStock(source, whatisResult.Value+" "+message)
 }
 
 func (p *Stock) handleStock(source *gobot.MessageSource, message string) {
